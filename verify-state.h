wildgarden/fio

commit e139c0c076e596143a13ce4cbf062120c30b338f

verify: escape '/' in state file name to '.'
If the job name includes a front slash, then we get an error when
we try to create the state file name:

	fio: open state file: No such file or directory

Change the '/' to '.' instead, so we generate a valid file name.

Signed-off-by: Jens Axboe <axboe@fb.com>

verify-state.h
@@ -2,6 +2,7 @@
 #define FIO_VERIFY_STATE_H
 
 #include <stdint.h>
+#include <string.h>
 
 struct thread_rand32_state {
 	uint32_t s[4];
@@ -82,8 +83,21 @@ static inline void verify_state_gen_name(char *out, size_t size,
 					 const char *name, const char *prefix,
 					 int num)
 {
+	char *ptr;
+
 	snprintf(out, size, "%s-%s-%d-verify.state", prefix, name, num);
 	out[size - 1] = '\0';
+
+	/*
+	 * Escape '/', just turn them into '.'
+	 */
+	ptr = out;
+	while ((ptr = strchr(ptr, '/')) != NULL) {
+		*ptr = '.';
+		ptr++;
+		if (*ptr == '\0')
+			break;
+	}
 }
 
 #endif

commit 7b07515373d22765614ada7de47083c40d4c019e

verify: only escape 'name', not prefix+name
Fixes: e139c0c076e5 ("verify: escape '/' in state file name to '.'")
Signed-off-by: Jens Axboe <axboe@fb.com>

verify-state.h
@@ -83,21 +83,24 @@ static inline void verify_state_gen_name(char *out, size_t size,
 					 const char *name, const char *prefix,
 					 int num)
 {
+	char ename[PATH_MAX];
 	char *ptr;
-
-	snprintf(out, size, "%s-%s-%d-verify.state", prefix, name, num);
-	out[size - 1] = '\0';
 
 	/*
 	 * Escape '/', just turn them into '.'
 	 */
-	ptr = out;
+	strcpy(ename, name);
+	ptr = ename;
 	while ((ptr = strchr(ptr, '/')) != NULL) {
 		*ptr = '.';
 		ptr++;
 		if (*ptr == '\0')
 			break;
 	}
+
+	snprintf(out, size, "%s-%s-%d-verify.state", prefix, ename, num);
+	out[size - 1] = '\0';
+
 }
 
 #endif